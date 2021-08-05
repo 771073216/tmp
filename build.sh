33f20f12cc1610261f456390309eb3fd95d739db
771073216 <37037387+771073216@users.noreply.github.com>
2021-08-06 00:27:46 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,9 +21,7 @@ setup() {
   done
 }
 
-main() {
-  cd community || exit 1
-  mkdir own
+build_cn(){
   setup cn
   findinclude
   curl -sSL $CHINA_DOMAINS_URL | perl -ne '/^server=\/([^\/]+)\// && print "$1\n"' >> out
@@ -32,10 +30,30 @@ main() {
   sed -i '/^#/d' out
   sed '/^$/d' out | sort --ignore-case -u > own/cn
   rm out
+}
+
+build_ad(){
+  setup category-ads
+  findinclude
+  sed -i '/^#/d' out
+  sed '/^$/d' out | sort --ignore-case -u > own/category-ads
+  rm out
+}
+
+build_ad_all(){
   setup category-ads-all
   findinclude
   sed -i '/^#/d' out
   sed '/^$/d' out | sort --ignore-case -u > own/category-ads-all
+  rm out
+}
+
+main() {
+  cd community || exit 1
+  mkdir own
+  build_cn
+  build_ad_all
+  build_ad
   go run ./ --datapath=own/ --outputname=geosite.dat --outputdir=../publish/
   cd ..
   rm -rf community
